735d5f25446576e09ce5a724427246784cea10a1
7canv <7777777canv@gmail.com>
2026-07-18 10:07:26 +0800

    refactor: 移除废弃的Restaurant模块

--- backend/src/main/java/com/foodadvisor/backend/mapper/RestaurantMapper.java
+++
@@ -1,9 +0,0 @@
-package com.foodadvisor.backend.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.foodadvisor.backend.entity.Restaurant;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface RestaurantMapper extends BaseMapper<Restaurant> {
-}
